60aeb46b710e817afdcd80a885bac74efd2d2326
Jon Woods <jonoflayham@gmail.com>
2015-10-17 19:36:24 +0100

    Persuade Travis to push site builds to the blog content repo

--- travis-test-and-push.sh
+++ travis-test-and-push.sh
@@ -3,7 +3,7 @@
 echo "Executing Midje tests"
 lein midje
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Travis pull request, so only building site, not pushing it"
     lein run -m blog-gen.web/export
     exit 0
@@ -11,7 +11,7 @@ fi
 
 generatorCommitHash=`git rev-parse HEAD`
 
-echo "Cloning existing site repo"
+echo "Cloning existing repo for the built site"
 mkdir dist
 cd dist
 git clone https://${GH_TOKEN}@github.com/jonoflayham/jonoflayham.github.io.git
@@ -19,9 +19,9 @@ git clone https://${GH_TOKEN}@github.com/jonoflayham/jonoflayham.github.io.git
 echo "Building site"
 lein run -m blog-gen.web/export
 
-echo "Adding, committing and pushing changes"
+echo "Adding, committing and pushing changes to the repo for the built site"
 git config user.email ${GH_EMAIL}
 git config user.name ${GH_NAME}
 git add --all
-git commit -m "Site generated from commit $generatorCommitHash"
+git commit -m "Site generated from https://github.com/jonoflayham/blog-gen/commit/$generatorCommitHash"
 git push https://${GH_TOKEN}@github.com/jonoflayham/jonoflayham.github.io.git
