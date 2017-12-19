8faf74270b4a3375e726363972547cfd37301491
Jon Woods <jonoflayham@gmail.com>
2017-12-19 19:15:47 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "http://jonoflayham.com"
   :license {:name "BSD 2 Clause"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [stasis "2.3.0"]
-                 [ring "1.6.1"]
+                 [ring "1.6.3"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [enlive "1.1.6"]
                  [clygments "1.0.0"]
-                 [optimus "0.19.3"]
-                 [clj-time "0.13.0"]
+                 [optimus "0.20.1"]
+                 [clj-time "0.14.2"]
                  [org.clojure/data.xml "0.0.8"]]
   :ring {:handler blog-gen.web/app}
   :aliases {"build-site" ["run" "-m" "blog-gen.web/export"]}
-  :profiles {:dev {:dependencies [[midje "1.8.3" :exclusions [org.clojure/clojure]]]
-                   :plugins [[lein-ring "0.9.7"]
+  :profiles {:dev {:dependencies [[midje "1.9.1" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-ring "0.12.2"]
                              [lein-midje "3.2"]
-                             [lein-ancient "0.6.10"]]}})
+                             [lein-ancient "0.6.15"]]}})
