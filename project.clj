b2c7f7e210f92aed8565de3675d9ff0b79a5029d
Jon Woods <jonoflayham@gmail.com>
2021-07-03 19:38:34 +0100

    Update music list and blog generator deps

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "http://jonoflayham.com"
   :license {:name "BSD 2 Clause"
             :url  "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [stasis "2.5.0"]
-                 [ring "1.7.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [stasis "2.5.1"]
+                 [ring "1.9.3"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [enlive "1.1.6"]
-                 [clygments "2.0.0"]
+                 [clygments "2.0.2"]
                  [optimus "0.20.2"]
                  [clj-time "0.15.2"]
                  [org.clojure/data.xml "0.0.8"]]
   :ring {:handler blog-gen.web/app}
   :aliases {"build-site" ["run" "-m" "blog-gen.web/export"]}
-  :profiles {:dev {:dependencies [[midje "1.9.9" :exclusions [org.clojure/clojure]]]
-                   :plugins      [[lein-ring "0.12.2"]
-                                  [lein-midje "3.2"]
-                                  [lein-ancient "0.6.15"]]}})
+  :profiles {:dev {:dependencies [[midje "1.10.3" :exclusions [org.clojure/clojure]]]
+                   :plugins      [[lein-ring "0.12.5"]
+                                  [lein-midje "3.2.1"]
+                                  [lein-ancient "1.0.0-RC3"]]}})
